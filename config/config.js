aa1fdef6ad6abb61d5574bcdfa8c117b05cd4ac7
♻ NOr._Nor ♻ <Im_Just_Non@hotmail.com>
2024-02-20 05:36:51 +0700

    Feat : try to fix fatal error

--- config/config.js
+++ config/config.js
@@ -13,6 +13,7 @@ module.exports = {
             password: process.env.MYSQL_PASSWORD,
             port: parseInt(process.env.MYSQL_PORT),
             database: process.env.MYSQL_DATABASE,
+            fatal: true
             // ssl: {
             //     rejectUnauthorized: false,
             // },
